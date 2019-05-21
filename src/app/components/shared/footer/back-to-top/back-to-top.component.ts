92873408f42df031725011ce0a513c8940fcbe3b
Adam Doe <adamnickolasdoe@gmail.com>
2019-05-21 09:43:18 -0700

    Add scroll to top button

--- src/app/components/shared/footer/back-to-top/back-to-top.component.ts
+++ src/app/components/shared/footer/back-to-top/back-to-top.component.ts
@@ -7,15 +7,38 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class BackToTopComponent implements OnInit {
 
+  showScroll: boolean;
+  showScrollHeight = 300;
+  hideScrollHeight = 10;
+
   constructor() { }
 
   ngOnInit() {
   }
-  @HostListener('window:scroll', ['$event']) 
-  scroll(event) {
-    // console.debug("Scroll Event", document.body.scrollTop);
-    // see András Szepesházi's comment below
-    console.debug("Scroll Event", window.pageYOffset );
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() 
+  {
+    if (( window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) > this.showScrollHeight) 
+    {
+      this.showScroll = true;
+    } 
+    else if ( this.showScroll && (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) < this.hideScrollHeight) 
+    { 
+      this.showScroll = false; 
+    }
   }
+
+  scrollToTop() 
+    { 
+      (function smoothscroll() 
+      { var currentScroll = document.documentElement.scrollTop || document.body.scrollTop; 
+        if (currentScroll > 0) 
+        {
+          window.requestAnimationFrame(smoothscroll);
+          window.scrollTo(0, currentScroll - (currentScroll / 5));
+        }
+      })();
+    }
 
 }
